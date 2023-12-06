12c96be5c10fa357ce5379f460ebf1c0c046ccd7
JanReisiegel <57211451+JanReisiegel@users.noreply.github.com>
2023-12-06 10:46:45 +0100

    DAy6-WaitForIt

---
+++ AoC2023/Day6_WaitForIt/Models/WaitForIt.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day6_WaitForIt.Models
+{
+    class WaitForIt
+    {
+        public List<long> Times { get; set; }
+        public List<long> Distances { get; set; }
+
+        public WaitForIt(string[] lines)
+        {
+            Times = Regex.Split(lines[0].Replace("\r",String.Empty), @"\W+").Skip(1).Select(long.Parse).ToList();
+            Distances = Regex.Split(lines[1].Replace("\r", String.Empty), @"\W+").Skip(1).Select(long.Parse).ToList();
+        }
+
+        public long PartOne()
+        {
+            List<long> wins = new List<long>();
+            foreach (var time in Times)
+            {
+                long win = 0;
+                for (long i = 0; i <= time; i++)
+                {
+                    long distance = (time - i) * i;
+                    if (distance > Distances[Times.IndexOf(time)])
+                    {
+                        win++;
+                    }
+                }
+                
+                wins.Add(win);
+            }
+            long result = 1;
+            foreach (var win in wins)
+            {
+                result = result * win;
+            }
+            return result;
+        }
+
+        public long PartTwo()
+        {
+            long time = long.Parse(String.Join("", Times));
+            long defDistance = long.Parse(String.Join("", Distances)); 
+            long result = 0;
+            for (long i = 0; i <= time; i++)
+            {
+                long distance = (time - i) * i;
+                if (distance > defDistance)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AoC2023/Day6_WaitForIt/Program.cs
@@ -0,0 +1,15 @@
+﻿using Day6_WaitForIt.Models;
+
+Console.WriteLine("-----------------------------------------------\n\n\tTest\n");
+var test = await File.ReadAllLinesAsync("E:/++AdventOfCode/AoC2023/Day6_WaitForIt/Test.txt"); //desktop
+//var test1 = await File.ReadAllTextAsync("D:/++AoC/AoC2023/Day6_WaitForIt/Test.txt");  //laptop
+WaitForIt wft1 = new WaitForIt(test);
+Console.WriteLine("PartOne: " + wft1.PartOne());
+Console.WriteLine("PartTwo: " + wft1.PartTwo());
+
+Console.WriteLine("-----------------------------------------------\n\n\tÚloha\n");
+var input = await File.ReadAllLinesAsync("E:/++AdventOfCode/AoC2023/Day6_WaitForIt/Puzzle.txt"); //desktop
+//var input = await File.ReadAllTextAsync("D:/++AoC/AoC2023/Day6_WaitForIt/Puzzle.txt"); //laptop
+WaitForIt wft2 = new WaitForIt(input);
+Console.WriteLine("PartOne: " + wft2.PartOne());
+Console.WriteLine("PartTwo: " + wft2.PartTwo());
