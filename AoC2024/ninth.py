db1ea59ffb8f71c01e908697ef214f93eefd37ac
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-12-09 12:09:12 +0100

    Day 9

--- AoC2024/ninth.py
+++ AoC2024/ninth.py
@@ -8,6 +8,7 @@ class FileSystem:
         '''Initiate file system'''
         self.disk = []
         self.moved_disk = []
+        self.checksum = []
         self.blocks = [int(x) for x in input_line]
 
     def disk_map(self):
@@ -25,11 +26,34 @@ class FileSystem:
             if change_id:
                 block_id += 1
             free_space = not free_space
+        with open('09/disk.txt', 'w', encoding='utf-8') as file:
+            file.write('[\'' + '\',\''.join(map(str, self.disk)) + '\']')
+            file.close()
+        # print(self.disk)
 
     def move_blocks(self):
         '''Move blocks'''
-        for disk_part_1 in self.disk:
-            pass
+        disk_len = len(self.disk)
+        last_index = disk_len - 1
+        for i in range(disk_len):
+            if i > last_index:
+                break
+            if self.disk[i] != '.':
+                self.moved_disk.append(self.disk[i])
+                self.checksum.append(self.disk[i] * i)
+                continue
+            not_inserted = True
+            while not_inserted:
+                if self.disk[last_index] == '.':
+                    last_index -= 1
+                else:
+                    self.moved_disk.append(self.disk[last_index])
+                    self.checksum.append(self.disk[last_index] * i)
+                    last_index -= 1
+                    not_inserted = False
+        with open('09/moved_disk.txt', 'w', encoding='utf-8') as file:
+            file.write('[\'' + '\',\''.join(map(str, self.moved_disk)) + '\']')
+            file.close()
 
 
 def worker_1(file_path):
@@ -39,9 +63,12 @@ def worker_1(file_path):
         file.close()
     fs = FileSystem(line)
     fs.disk_map()
-    return 1
+    # print(fs.disk)
+    fs.move_blocks()
+    # print(fs.moved_disk)
+    return sum(fs.checksum)
 
 
 if __name__ == '__main__':
     print('Example part 1:', worker_1('09/example.txt'))
-    # print('Part 1:', worker_1('09/puzzle.txt'))
+    print('Part 1:', worker_1('09/puzzle.txt'))
