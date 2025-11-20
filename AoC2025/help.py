78c55d6fe31ee85d1fc2e24e6602c2818614cc4e
Jan_Reisiegel <jan.reisiegel@tul.cz>
2025-11-20 22:45:32 +0100

    Prepare fo AoC 2025

---
+++ AoC2025/help.py
@@ -0,0 +1,7 @@
+'''file with helper functions for AoC 2025 challenges'''
+
+
+def read_input(file_path):
+    """Reads the input file and returns its content as a list of lines."""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file.readlines()]
