51cdf2e133ae7434e3b93fdf7879ef2f80fee656
Jan_Reisiegel <jan.reisiegel@tul.cz>
2025-12-07 10:51:20 +0100

    day 7 changes

--- AoC2025/src/day_07/tachyon_beams.py
+++ AoC2025/src/day_07/tachyon_beams.py
@@ -1,4 +1,5 @@
 '''Class for Tachyon Beams problem'''
+from pprint import pprint
 
 
 class TachyonBeams:
@@ -11,10 +12,18 @@ class TachyonBeams:
         '''Solves part 1 of the Tachyon Beams problem.'''
         result = 0
         # Implementation for solving part 1 goes here
-        row_number = len(self.grid)-1
+        row_number = len(self.grid)
         col_number = len(self.grid[0])
-        for i in range(row_number):
-            for j in range(col_number):
-                if self.grid[i][j] == 'S':
-                    self.grid[i+1][j] = '|'
+        s_index = self.grid[0].index('S')
+        self.grid[1][s_index] = '|'
+        print(col_number, row_number)
+        new_grid = [self.grid[i] for i in range(0, row_number, 2)]
+        row_number = len(new_grid)
+        for r in range(1, row_number):
+            for c in range(col_number):
+                if new_grid[r][c] == '^':
+                    new_grid[r][c-1] = "|"
+                    new_grid[r][c+1] = "|"
+                    if new_grid[r-1][c] == '|' or new_grid[r-1][c] == 'S':
+                        result += 1
         return result
